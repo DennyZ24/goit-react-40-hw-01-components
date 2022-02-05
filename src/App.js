f8af4febbaa57fb017232ee8fd2c1d7c58bd7ddb
Denis Zapara <dzapara24@gmail.com>
2022-02-05 17:14:31 +0300

    make components for my hw without style

--- src/App.js
+++
@@ -1,8 +0,0 @@
-
-function App() {
-  return (
-    <div>hellow</div>
-  );
-}
-
-export default App;
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types'
+
+export default function Statistics(props) {
+  const { title, stats } = props;
+
+  return (
+  <section class="statistics">
+    {title && <h2 class="title">{title}</h2>}
+
+    <ul class="stat-list">
+        {stats.map(el => {
+          const { id, label, percentage } = el;
+          return (
+            <li class="item" key={id}>
+              <span class="label">{label}</span>
+              <span class="percentage">{percentage}</span>
+            </li>
+        )
+      })}
+    </ul>
+  </section>
+  )
+}
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    }),
+  ),
+}
